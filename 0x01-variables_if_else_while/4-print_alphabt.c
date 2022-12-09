b1e09fa41ec23bccf6358eacb5c6563a618f1326
AhmedShams40 <117733784+AhmedShams40@users.noreply.github.com>
2022-12-09 14:38:00 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,10 @@ int main(void)
 
 	for (a = 'a'; a <= 'z'; a++)
 	{
-        if(a != 'q' && a != 'e')
-		putchar(a);
+		if ((a != 'e') && (a != 'q'))
+		{
+			putchar(a);
+		}
 	}
 	putchar('\n');
 	return (0);
