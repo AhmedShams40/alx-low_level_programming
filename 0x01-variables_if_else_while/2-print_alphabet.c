6c2731abe2da21c892d3e28812b38a1550feb967
AhmedShams40 <117733784+AhmedShams40@users.noreply.github.com>
2022-12-09 12:36:39 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-    for(int x = 'A'; x <= 'Z'; x++)
-    {
-    putchar(tolower(x));
-    }
-    return 0;
-}
+/**
+* main - Lanzador.
+*
+* Return: Always 0.
+*/
 
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
