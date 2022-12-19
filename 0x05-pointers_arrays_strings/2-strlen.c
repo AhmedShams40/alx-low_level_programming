052de1955b9c05e2d5c1b192e0a9f5bea34c35da
AhmedShams40 <117733784+AhmedShams40@users.noreply.github.com>
2022-12-19 12:50:53 +0200

    ahmed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that swaps the values of two integers.
+ * @s: input string.
+ * Return: no return.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{}
+
+	return (i);
+}
